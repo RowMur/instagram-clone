9138b438a82c492e10a3bebfe6e739a2d1c00b26
Rowan Murray <rowanomurray@gmail.com>
2024-03-20 17:11:57 +0000

    chore: setup gqlgen

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"database/sql"
 	"github/rowmur/insta-clone/internal/database"
+	"github/rowmur/insta-clone/internal/graph"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
@@ -43,6 +46,10 @@ func main() {
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(version))
 	})
+
+	gqlSrv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/playground", playground.Handler("GraphQL Playground", "/query"))
+	router.Handle("/query", gqlSrv)
 
 	http.ListenAndServe(":8080", router)
 }
