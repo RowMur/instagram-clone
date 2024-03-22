e341b73c4f45b5464827e12a41844efb64d69d80
Rowan Murray <rowanomurray@gmail.com>
2024-03-22 15:37:58 +0000

    refactor: add user follow metadata + refactor resolvers

---
+++ internal/graph/follows.go
@@ -0,0 +1,50 @@
+package graph
+
+import (
+	"context"
+	"fmt"
+	"github/rowmur/insta-clone/internal/database"
+	"github/rowmur/insta-clone/internal/graph/model"
+
+	"github.com/google/uuid"
+)
+
+func userFollows(r *userResolver, ctx context.Context, obj *model.User, following bool) ([]*model.Follow, error) {
+	currentUserId := uuid.MustParse(obj.ID)
+
+	var dbFollows []database.Follow
+	var err error
+	if following {
+		dbFollows, err = r.DBQueries.GetFollowsByUser(ctx, currentUserId)
+	} else {
+		dbFollows, err = r.DBQueries.GetFollowersByUser(ctx, currentUserId)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("something went wrong")
+	}
+
+	follows := []*model.Follow{}
+	for _, dbFollow := range dbFollows {
+		var otherUserId uuid.UUID
+		if following {
+			otherUserId = dbFollow.UserFollowingID
+		} else {
+			otherUserId = dbFollow.UserID
+		}
+
+		dbUser, err := r.DBQueries.GetUserById(ctx, otherUserId)
+		if err != nil {
+			return nil, fmt.Errorf("something went wrong")
+		}
+
+		user := dbUserToGqlUser(dbUser)
+		follow := model.Follow{
+			CreatedAt: dbFollow.CreatedAt.String(),
+			User:      &user,
+		}
+
+		follows = append(follows, &follow)
+	}
+
+	return follows, nil
+}
