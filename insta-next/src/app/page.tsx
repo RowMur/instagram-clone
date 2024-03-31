6525f5bdb17d40654bf3912e21013ed5e4bb2fb3
Rowan Murray <rowanomurray@gmail.com>
2024-03-31 23:40:31 +0100

    feat: add create user form

--- insta-next/src/app/page.tsx
+++ insta-next/src/app/page.tsx
@@ -1,5 +1,11 @@
 import TestComponent from "@/components/Component";
+import SignUpForm from "@/features/user/SignUpForm";
 
 export default function Home() {
-  return <TestComponent />
+  return (
+    <>
+      <SignUpForm />
+      <TestComponent />
+    </>
+  );
 }
---
+++ insta-next/src/features/user/SignUpForm.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { graphql } from "../../../gql";
+import request from "graphql-request";
+import { useForm } from "@tanstack/react-form";
+
+const createNewUserDocument = graphql(`
+  mutation CreateUser($name: String!) {
+    createUser(name: $name) {
+      apiKey
+    }
+  }
+`);
+
+const SignUpForm = () => {
+  const queryClient = useQueryClient();
+  const createUserMutation = useMutation({
+    mutationFn: async ({ name }: { name: string }) =>
+      request("http://localhost:8080/query", createNewUserDocument, {
+        name: name,
+      }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["users"],
+      });
+    },
+  });
+
+  const form = useForm({
+    defaultValues: {
+      name: "",
+    },
+    onSubmit: async ({ value }) => {
+      createUserMutation.mutate(value);
+    },
+  });
+  return (
+    <>
+      Sign Up
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          void form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="name"
+          children={(field) => (
+            <>
+              <input
+                type="text"
+                className="border-2 border-red-400"
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
+            </>
+          )}
+        />
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
