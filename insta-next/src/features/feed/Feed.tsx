22c70ea9a4b2f47646e528f9eb6d8a7538ddfdbb
Rowan Murray <rowanomurray@gmail.com>
2024-06-08 21:09:35 +0100

    chore: undo split of query

--- insta-next/src/features/feed/Feed.tsx
+++ insta-next/src/features/feed/Feed.tsx
@@ -2,8 +2,18 @@
 
 import { useQuery } from "@tanstack/react-query";
 import PostForm from "./PostForm";
-import { fetchFeed } from "./lib";
 import Post from "./Post";
+import request from "graphql-request";
+import { graphql } from "../../../gql";
+
+const feedDocument = graphql(`
+  query Feed {
+    posts {
+      id
+      ...Post
+    }
+  }
+`);
 
 type Props = {
   currentUser?: string;
@@ -12,7 +22,8 @@ type Props = {
 const Feed = (props: Props) => {
   const { data } = useQuery({
     queryKey: ["feed"],
-    queryFn: fetchFeed,
+    queryFn: () =>
+      request("http://localhost:8080/query", feedDocument, undefined),
   });
   return (
     <section className="my-10">
